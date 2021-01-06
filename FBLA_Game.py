f31c073e4c6240cbdb3bc2b7ea34c02a1318c081
bradyehman <brady.ehman@gmail.com>
2021-01-05 18:33:58 -0600

    Tuesday, Jan 5, 2021

--- FBLA_Game.py
+++ FBLA_Game.py
@@ -1,1 +1,29 @@
+import pygame
+from pygame.locals import *
 
+pygame.init()
+
+screen_width = 1000
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+
+# load images
+sun_img = pygame.image.load('sun.png')
+bg_img = pygame.image.load('sky.png')
+
+run = True
+while run:
+
+    screen.blit(bg_img, (0, 0))
+    screen.blit(sun_img, (100, 100))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
