b2e7fe873a663589a2a3d64a13da49ff00905187
bradyehman <brady.ehman@gmail.com>
2021-01-06 19:27:04 -0600

    Wednesday, Jan 6, 2021; Added player movement, collision, and first enemy.

--- FBLA_Game.py
+++ FBLA_Game.py
@@ -3,22 +3,83 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 1000
-screen_height = 800
+# squares in grid are 33 pixels
+screen_width = 1300
+screen_height = 750
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Platformer')
-
+pygame.display.set_caption('FBLA Project')
 
 # load images
-sun_img = pygame.image.load('sun.png')
-bg_img = pygame.image.load('sky.png')
+background = pygame.image.load('background.jpg')
+
+
+class Player():
+    def __init__(self, x, y):
+        img = pygame.image.load('player.png')
+        self.image = pygame.transform.scale(img, (33, 33))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+
+        # get keypresses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            self.rect.x -= 1
+        elif key[pygame.K_RIGHT]:
+            self.rect.x += 1
+        elif key[pygame.K_UP]:
+            self.rect.y -= 1
+        elif key[pygame.K_DOWN]:
+            self.rect.y += 1
+
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+        elif self.rect.top < 0:
+            self.rect.top = 0
+        elif self.rect.right > screen_width:
+            self.rect.right = screen_width
+        elif self.rect.left < 0:
+            self.rect.left = 0
+
+        # draw player onto screen
+        screen.blit(self.image, self.rect)
+
+
+player = Player(10, 375)
+
+
+class Obstacle():
+    def __init__(self, x, y):
+        img = pygame.image.load('obstacle.png')
+        self.image = pygame.transform.scale(img, (66, 66))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+
+    def update(self):
+        self.rect.y -= self.move_direction
+        self.move_counter += 1
+        if self.move_counter > 570:
+            self.move_direction *= -1
+            self.move_counter = 0
+
+        screen.blit(self.image, self.rect)
+
+
+obstacle = Obstacle(650, 600)
 
 run = True
 while run:
 
-    screen.blit(bg_img, (0, 0))
-    screen.blit(sun_img, (100, 100))
+    screen.blit(background, (0, 0))
+
+    player.update()
+    obstacle.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
