6a840d1ea4aea8442e4a21a47fcb1dcf7cb953a7
bradyehman <brady.ehman@gmail.com>
2021-02-28 18:17:08 -0600

    Sunday, Feb 28, 2021; Fixed problems with sprites, switched collisions to collisions with groups to save time, and further stabilized level system.

--- FBLA_Game.py
+++ FBLA_Game.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import pickle
 
 pygame.init()
 
@@ -110,11 +111,7 @@ class Player(pygame.sprite.Sprite):
                 self.rect.left = 0
 
             # checks for collision with enemies
-            if pygame.sprite.collide_mask(self, obstacle1):
-                self.rect.x = x
-                self.rect.y = y
-                life_count -= 1
-            if pygame.sprite.collide_mask(self, obstacle2):
+            if pygame.sprite.spritecollide(self, level1_obstacle_group, False):
                 self.rect.x = x
                 self.rect.y = y
                 life_count -= 1
@@ -130,52 +127,73 @@ player = Player(10, screen_height/2)
 
 # class for obstacles
 class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, x, y, move_direction):
+    def __init__(self, x, y, move_directionY, move_directionX, move_counter):
+        pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load('obstacle.png')
         self.image = pygame.transform.scale(img, (66, 66))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.move_direction = move_direction
-        self.move_counter = 0
+        self.move_directionY = move_directionY
+        self.move_directionX = move_directionX
+        self.start_move_counter = 0
+        self.move_counter = move_counter
 
     def update(self):
-        self.rect.y -= self.move_direction
-        self.move_counter += 1
-        if self.move_counter > screen_height - 66:
-            self.move_direction *= -1
-            self.move_counter = 0
+        self.rect.y -= self.move_directionY
+        self.rect.x -= self.move_directionX
+        self.start_move_counter += 1
+        if self.start_move_counter > self.move_counter:
+            self.move_directionY *= -1
+            self.move_directionX *= -1
+            self.start_move_counter = 0
 
         screen.blit(self.image, self.rect)
 
+# Obstacles for level 1
+level1_obstacle_group = pygame.sprite.Group()
+level1_obstacle1 = Obstacle(screen_width/2 + 33, screen_height - 66, 1, 0, screen_height - 66)
+level1_obstacle_group.add(level1_obstacle1)
+level1_obstacle2 = Obstacle(screen_width/2 - 99, 0, -1, 0, screen_height - 66)
+level1_obstacle_group.add(level1_obstacle2)
+level1_obstacle3 = Obstacle(screen_width/2 + 500, screen_height - 300, 0, 2, (screen_width - 200)/2)
+level1_obstacle_group.add(level1_obstacle3)
+level1_obstacle4 = Obstacle(screen_width/2 + 500, screen_height - 500, 0, 2, (screen_width - 200)/2)
+level1_obstacle_group.add(level1_obstacle4)
 
-obstacle1 = Obstacle(screen_width/2 + 33, screen_height - 66, 1)
-obstacle2 = Obstacle(screen_width/2 - 66, 0, -1)
+# Obstacles for level 2
+level2_obstacle_group = pygame.sprite.Group()
+level2_obstacle1 = Obstacle()
+level2_obstacle_group.add(level2_obstacle1)
+level2_obstacle2 = Obstacle()
+level2_obstacle_group.add(level2_obstacle2)
+level2_obstacle3 = Obstacle()
+level2_obstacle_group.add(level2_obstacle3)
+level2_obstacle4 = Obstacle()
+level2_obstacle_group.add(level2_obstacle4)
 
 
 # class for coins
-coin1_collected = False
-
-
 class Coin(pygame.sprite.Sprite):
     def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load('coin.png')
         self.image = pygame.transform.scale(img, (33, 33))
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-    def update(self, coin1_collected, score_value):
-        if not coin1_collected:
-            screen.blit(self.image, self.rect)
-            if pygame.sprite.collide_mask(self, player):
-                coin1_collected = True
-                score_value += 1/2
-                show_score(score_textX, score_textY)
-        return score_value
+        self.rect.center = (x, y)
 
 
-coin1 = Coin(screen_width / 2, screen_height / 2)
+level1_coin_group = pygame.sprite.Group()
+level1_coin1 = Coin(screen_width / 2, screen_height / 2)
+level1_coin_group.add(level1_coin1)
+level1_coin2 = Coin(100, 100)
+level1_coin_group.add(level1_coin2)
+level1_coin3 = Coin(screen_width - 100, 100)
+level1_coin_group.add(level1_coin3)
+level1_coin4 = Coin(100, screen_height - 100)
+level1_coin_group.add(level1_coin4)
+level1_coin5 = Coin(screen_width - 100, screen_height - 100)
+level1_coin_group.add(level1_coin5)
 
 
 # class for finish line
@@ -184,24 +202,26 @@ level_completed = False
 
 class FinishLine(pygame.sprite.Sprite):
     def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load('Finish line.png')
         self.image = pygame.transform.scale(img, (50, screen_height - 100))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
-    def update(self, level_completed):
+    '''def update(self, level_completed):
         screen.blit(self.image, self.rect)
         if not level_completed:
             if pygame.sprite.collide_mask(self, player):
                 level_completed = True
                 player.rect.x = 10
                 player.rect.y = screen_height/2
-        return level_completed
+        return level_completed'''
 
 
+finish_line_group = pygame.sprite.Group()
 finish_line = FinishLine(screen_width - 50, screen_height/2 - (screen_height - 100)/2)
-
+finish_line_group.add(finish_line)
 
 # game over text
 game_over_font = pygame.font.Font('freesansbold.ttf', 64)
@@ -242,19 +262,25 @@ while run:
         show_lives(life_textX, life_textY)
         if level == 1:
             life_count = player.update(life_count, 10, screen_height/2)
+            level1_obstacle_group.draw(screen)
+            level1_obstacle_group.update()
+            level1_coin_group.draw(screen)
+            finish_line_group.draw(screen)
+            if pygame.sprite.spritecollide(player, level1_coin_group, True):
+                score_value += 1
+            if pygame.sprite.spritecollide(player, finish_line_group, False):
+                player.rect.x = 10
+                player.rect.y = screen_height / 2
+                level1_obstacle_group.remove(level1_obstacle1, level1_obstacle2, level1_obstacle3, level1_obstacle4)
+                level += 1
         if level == 2:
             life_count = player.update(life_count, 10, screen_height/2)
         if level == 3:
             life_count = player.update(life_count, 10, screen_height/2)
-        obstacle1.update()
-        obstacle2.update()
-        coin1_collected = coin1.update(coin1_collected, int(score_value))
-        score_value = coin1.update(int(score_value), coin1_collected)
-        level_completed = finish_line.update(level_completed)
+        '''level_completed = finish_line.update(level_completed)
         if level_completed == True:
             level += 1
-            print(level)
-            level_completed = False
+            level_completed = False'''
         if life_count == 0:
             screen.blit(background, (0, 0))
             print_game_over(game_overX, game_overY)
